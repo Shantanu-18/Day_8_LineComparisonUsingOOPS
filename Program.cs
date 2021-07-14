cee042a08b85271f52eec909384e9813c7681ab5
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-14 18:04:32 +0530

    [Shantanu] Added : Main program file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LineCompUsingOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            //UC_1.LenghtOfSingleLine();
+            //UC_2.CheckEquality();
+            //UC_3.CompareToMethod();
+
+            UC_4 obj = new UC_4();
+            obj.UserInput();
+            obj.calculation();
+            obj.compare();
+
+            
+
+
+            
+        }
+    }
+}
